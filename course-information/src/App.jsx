76d1e30c4f737b3af972cf77f470cfb1349a0369
Andres Torres <andrestor2@gmail.com>
2024-02-26 00:14:39 -0500

    Change object structure and solve problems.

--- course-information/src/App.jsx
+++ course-information/src/App.jsx
@@ -1,9 +1,10 @@
 const Header = ({ course }) => {
   console.log(course);
-  return <h1 className="Header">{course}</h1>;
+  return <h1 className="Header">{course.name}</h1>;
 };
 
-const Content = ({ parts }) => {
+const Content = ({ course }) => {
+  const {parts} = course;
   const part1 = parts[0];
   const part2 = parts[1];
   const part3 = parts[2];
@@ -22,33 +23,36 @@ const Content = ({ parts }) => {
   );
 };
 
-const Total = ({ parts }) => {
+const Total = ({ course }) => {
+  const {parts} = course;
   const total = parts.reduce((prev, curr) => curr.exercises + prev, 0);
   return <p>Number of exercises {total}</p>;
 };
 
 const App = () => {
-  const course = 'Half Stack application development';
-  const parts = [
-    {
-      name: 'Fundamentals of React',
-      exercises: 10,
-    },
-    {
-      name: 'Using props to pass data',
-      exercises: 7,
-    },
-    {
-      name: 'State of a component',
-      exercises: 14,
-    },
-  ];
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
 
   return (
     <div>
       <Header course={course} />
-      <Content parts={parts} />
-      <Total parts={parts} />
+      <Content course={course} />
+      <Total course={course} />
     </div>
   );
 };
